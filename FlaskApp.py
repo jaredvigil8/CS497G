89b758fb5d66697e8ec39480470ed1545e1b6ff9
zdshaw <zshaws@yahoo.com>
2017-05-29 13:36:42 -0700

    Added input validation for donate and request

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,10 +1,69 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, url_for, redirect
+import sqlite3 as sql
+
+
 app = Flask(__name__)
+app.config.from_object('config')
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
+@app.route('/index')
+def home():
+	return render_template('index.html')
+
+@app.route('/enternew')
+def new_student():
+	return render_template('student.html')
+
+
+@app.route('/enternewrequest')
+def new_request():
+	return render_template('request.html')
+
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+	message = "Before record insertion"
+	if request.method == 'POST':
+		try:
+			nm = request.form['nm']
+			addr = request.form['add']
+			city = request.form['city']
+			pin = request.form['pin']
+			Type = request.form['Type']
+			with sql.connect("database.db") as con:
+				cur = con.cursor()
+				cur.execute("INSERT INTO allItems (name,addr,city,pin,Type) VALUES (?,?,?,?,?)",(nm,addr,city,pin,Type) 
+)
+				con.commit()
+				message = "Record successfully added into "+ Type
+			with sql.connect("database.db") as con:
+				cur = con.cursor()
+				cur.execute("INSERT INTO "+Type+" (name,addr,city,pin,Type) VALUES (?,?,?,?,?)",(nm,addr,city,pin,Type))
+				con.commit()
+				message = "Record successfully added into "+ Type
+		except:
+			con.rollback()
+			message = "Error in insert operation"
+
+		finally:
+			return render_template("result.html", msg = message)
+			con.close()
+
+@app.route('/list')
+def list():
+	con = sql.connect("database.db")	
+	con.row_factory = sql.Row
+	
+	cur = con.cursor()
+	cur.execute("select * from allItems")
+
+	rows = cur.fetchall()
+	return render_template('list.html', rows = rows)
+
+	
 @app.route('/Donate')
 def Donate():
     return render_template('donate.html')
@@ -20,30 +79,70 @@ def Tutorial():
 @app.route('/Signup')
 def Signup():
     return render_template('signup.html')
-
 @app.route('/showFood')
-def showFood():
-    return render_template('searchResults.html')
+def showFood(): 
+	con = sql.connect("database.db")	
+	con.row_factory = sql.Row
+	
+	cur = con.cursor()
+	cur.execute("select * from food")
+
+	rows = cur.fetchall()
+	return render_template('list.html', rows = rows)
 
 @app.route('/showHygiene')
 def showHygiene():
-    return render_template('searchResults.html')
+	con = sql.connect("database.db")	
+	con.row_factory = sql.Row
+	
+	cur = con.cursor()
+	cur.execute("select * from hygiene")
+
+	rows = cur.fetchall()
+	return render_template('list.html', rows = rows)
 
 @app.route('/showOddjobs')
 def showOddjobs():
-    return render_template('searchResults.html')
+	con = sql.connect("database.db")	
+	con.row_factory = sql.Row
+	
+	cur = con.cursor()
+	cur.execute("select * from jobs")
 
+	rows = cur.fetchall()
+	return render_template('list.html', rows = rows)
 @app.route('/showClothes')
 def showClothes():
-    return render_template('searchResults.html')
+	con = sql.connect("database.db")	
+	con.row_factory = sql.Row
+	
+	cur = con.cursor()
+	cur.execute("select * from clothes")
+
+	rows = cur.fetchall()
+	return render_template('list.html', rows = rows)
 
 @app.route('/showShelter')
 def showShelter():
-    return render_template('searchResults.html')
+	con = sql.connect("database.db")	
+	con.row_factory = sql.Row
+	
+	cur = con.cursor()
+	cur.execute("select * from shelter")
+
+	rows = cur.fetchall()
+	return render_template('list.html', rows = rows)
 
 @app.route('/showTransportation')
 def showTransportation():
-    return render_template('searchResults.html')
+	con = sql.connect("database.db")	
+	con.row_factory = sql.Row
+	
+	cur = con.cursor()
+	cur.execute("select * from transportation")
+
+	rows = cur.fetchall()
+	return render_template('list.html', rows = rows)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
